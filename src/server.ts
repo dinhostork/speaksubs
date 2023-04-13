65504a8f757fd93a1d3a93ae304f7a8dea123cd9
Dinho Stork <dinhostork@live.com>
2023-04-13 14:53:38 -0300

    feat: routes config

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,30 @@
 import express from 'express';
-//import { router } from './application/routes';
+import router from './application/routes';
 import { connect } from './infrastructure/database/mongodb.config';
 
 class Server {
   private app: express.Application;
   private readonly port: number;
+  private version: number | string;
+  private url: string;
 
   constructor() {
     this.app = express();
     this.port = parseInt(process.env.APLICATION_PORT || '3000');
+    this.version = process.env.API_VERSION || 1;
+    this.url = process.env.API_URL || 'localhost';
 
     connect();
     
     this.app.use(express.json());
 
-    //this.app.use('/', router);
+    this.app.use(`/v${this.version}`, router);
   }
 
   public start() {
     // Inicia o servidor
     this.app.listen(this.port, () => {
-      console.log(`Servidor rodando na porta ${this.port}`);
+      console.log(`Servidor rodando em ${this.url}:${this.port}/v${this.version}`);
     });
   }
 }
